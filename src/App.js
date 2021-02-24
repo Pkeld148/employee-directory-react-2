9f84c49ab1108e5f53eab1fbe86217a14bcd1b6c
Paul Keldsen <Pkeld148@gmail.com>
2021-02-24 12:40:42 -0500

    Revert "Updates"
    This reverts commit 4cd3b5b36a40e26644fcc21af6c4c8578e9e2974.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Employees from "./components/Employees/Employees";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Employee Directory</h1>
+      <Employees />
+    </div>
+  );
+}
+
+export default App;
